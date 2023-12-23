19cf387c8cca9c15ee1e10a82c2bd19aa5be22dc
Ian M. Nava Garduno <ianmng1@gmail.com>
2023-12-22 23:11:27 -0800

    Index/JS: Carousel finished

--- index/script.js
+++ index/script.js
@@ -1,9 +1,8 @@
 "use strict";
 
-// CAROUSEL
+// START CAROUSEL
 
 // Elements selection
-const btns = document.querySelectorAll(".btn");
 const btnRight = document.querySelector(".btn-right");
 const btnLeft = document.querySelector(".btn-left");
 
@@ -11,97 +10,29 @@ const boloCar = document.querySelector(".bolo-car");
 const pastaCar = document.querySelector(".pasta-car");
 const alfCar = document.querySelector(".alf-car");
 
-const allCarousels = document.querySelectorAll(".car"); // Nodelist (array)
+const switchCarousel = function (car1, car2) {
+  car1.classList.toggle("hidden");
+  car2.classList.toggle("hidden");
+};
 
 btnRight.addEventListener("click", () => {
   if (!pastaCar.classList.contains("hidden")) {
-    pastaCar.classList.add("hidden");
-    alfCar.classList.remove("hidden");
+    switchCarousel(pastaCar, alfCar);
   } else if (!alfCar.classList.contains("hidden")) {
-    alfCar.classList.add("hidden");
-    boloCar.classList.remove("hidden");
+    switchCarousel(alfCar, boloCar);
   } else if (!boloCar.classList.contains("hidden")) {
-    boloCar.classList.add("hidden");
-    pastaCar.classList.remove("hidden");
+    switchCarousel(boloCar, pastaCar);
   }
 });
 
 btnLeft.addEventListener("click", () => {
   if (!pastaCar.classList.contains("hidden")) {
-    pastaCar.classList.add("hidden");
-    boloCar.classList.remove("hidden");
+    switchCarousel(pastaCar, boloCar);
   } else if (!boloCar.classList.contains("hidden")) {
-    boloCar.classList.add("hidden");
-    alfCar.classList.remove("hidden");
+    switchCarousel(boloCar, alfCar);
   } else if (!alfCar.classList.contains("hidden")) {
-    alfCar.classList.add("hidden");
-    pastaCar.classList.remove("hidden");
+    switchCarousel(alfCar, pastaCar);
   }
 });
-/*
-
-// function to switch car components
-const switchCars = function (firstCar, secondCar) {
-  firstCar.classList.add("hidden");
-  secondCar.classList.remove("hidden");
-};
-
-// Bolognese element
-const btnRBol = document.querySelector(".btn-r-bol");
-const btnLBol = document.querySelector(".btn-l-bol");
-
-btnRBol.addEventListener("click", () => {
-  // boloCar.classList.add("hidden");
-  // difTypeCar.classList.remove("hidden");
-
-  switchCars(boloCar, difTypeCar);
-});
-
-btnLBol.addEventListener("click", () => {
-  // boloCar.classList.add("hidden");
-  // alfCar.classList.remove("hidden");
-
-  switchCars(boloCar, alfCar);
-});
-
-// Pasta element
-const difTypeCar = document.querySelector(".type-pas-car");
-const btnRPas = document.querySelector(".btn-r-pas");
-const btnLPas = document.querySelector(".btn-l-pas");
-
-btnRPas.addEventListener("click", () => {
-  // difTypeCar.classList.add("hidden");
-  // alfCar.classList.remove("hidden");
-
-  switchCars(difTypeCar, alfCar);
-});
-
-btnLPas.addEventListener("click", () => {
-  // difTypeCar.classList.add("hidden");
-  // boloCar.classList.remove("hidden");
-
-  switchCars(difTypeCar, boloCar);
-});
-
-// Alfredo element
-const alfCar = document.querySelector(".alf-car");
-const btnRAl = document.querySelector(".btn-r-al");
-const btnLAl = document.querySelector(".btn-l-al");
-
-btnRAl.addEventListener("click", () => {
-  // alfCar.classList.add("hidden");
-  // boloCar.classList.remove("hidden");
-
-  switchCars(alfCar, boloCar);
-});
-
-btnLAl.addEventListener("click", () => {
-  // alfCar.classList.add("hidden");
-  // difTypeCar.classList.remove("hidden");
-
-  switchCars(alfCar, difTypeCar);
-});
-
-*/
 
 //  END OF CAROUSEL
